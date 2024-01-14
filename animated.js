3246411f426c9739e00cc9bd69fe3288158bda78
shoeb1989 <shoebsikder1@gmail.com>
2024-01-14 21:06:07 +0600

    ain

---
+++ animated.js
@@ -0,0 +1,139 @@
+
+// var onionPrice = 10;
+// var eggPrice = 20;
+
+// var totalPrice = onionPrice + eggPrice ;
+
+
+// var price = 100;
+// console.log(typeof price);
+
+// var happy = true
+// console.log(typeof happy);
+
+// var name = 'shoeb'
+// console.log(typeof name);
+
+// var shoeb;
+// console.log(shoeb);
+
+// var mango = 7;
+// var person = 2;
+
+// console.log(mango % person);
+
+// var money = 1000;
+// var organge = 700;
+// var apple = 300;
+
+// var applePrice = (1000-300);
+// var organgePrice = (money - applePrice);
+
+// console.log(money);
+// console.log(applePrice);
+// console.log(organgePrice);
+
+// var math = 75.25;
+// var bio = 65;
+// var che = 80;
+// var phy = 35.45;
+// var ban = 99.50;
+// var cal = 5;
+
+// var totalNumber = ( math + bio + che + phy + ban);
+// var result = (totalNumber / cal);
+
+// var number = result.toFixed(2);
+// console.log(number);
+
+//arryy
+
+// var age = [12, 23 ,14 ,15 ,16];
+// // console.log(age.length); 
+// // console.log(age[1]);
+// var index = age.indexOf(23)
+// // console.log(index);
+
+// // var posIndex = age.indexOf(16);
+// // console.log(posIndex); 
+
+var number = [78, 45, 98, 45];
+// console.log(number[2]);
+// number [2] = 86;
+// console.log(number);
+
+// var find = number.indexOf(86);
+// console.log(find);
+
+// number.push(54);
+// console.log(number);
+// number.pop();
+// console.log(number);
+
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(6 != 7);
+
+// console.log(6 <= 7);
+
+
+// var phone = 79000;
+// var myBudget = 15000;
+
+// if (phone < myBudget){
+//     console.log('Kinbo');
+// }
+ 
+// else{
+//     console.log('kinbo na');
+// }
+
+// var tk = 100;
+// var car = 75;
+// var bike = 58;
+// var cycle = 30;
+
+// if(car < tk){
+//     console.log('car den');
+// }
+
+// else if( bike < tk){
+//     console.log('bike den');
+// }
+
+// else if(cycle < tk){
+//     console.log('cycle den');
+// }
+
+// else {
+//     console.log( 'kicu nibo na');
+// }
+
+// var number = 1;
+
+// while( number <= 7){
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// var number = [45, 87, 89, 56, 32, 51, 25, 85, 100];
+
+// for(var i = 0; i < number.length; i++){
+//     var numbers = number[i];
+//     console.log(numbers);
+// }
+
+// var num =0;
+// while(num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// var num = 10;
+// while (num >= 1){
+//     console.log(num);
+//     num--;
+// }
+
